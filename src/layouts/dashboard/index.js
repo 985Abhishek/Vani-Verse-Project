5694a3a2262b30dd598bdda6d56a58cb11d37faa
Abhishek Dhange <abhishekdhange@gmail.com>
2024-04-22 15:22:27 +0530

    side bar done

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,110 +1,125 @@
-import { Box, Divider, IconButton, Stack } from "@mui/material";
-//import { SelectionBackground } from "phosphor-react";
-import React from "react";
+import { Avatar, Box, Divider, IconButton, Stack, Switch } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
+import React from "react";
 import { Outlet } from "react-router-dom";
-
-import Logo from "../../assets/Images/logo.ico";
-import { Columns, Gear } from "phosphor-react";
+import logo from "../../assets/Images/logo.ico"; //from nowonwards import images things without curly braces it'll not work
 import { Nav_Buttons } from "../../data";
+import { Gear } from "phosphor-react";
 import { useState } from "react";
-import { Faker } from "@faker-js/faker";
+import { faker } from "@faker-js/faker";
+
+import useSettings from "../../hooks/useSettings";
 
 const DashboardLayout = () => {
   const theme = useTheme();
-  const [selected, setSelected] = useState(0);
   console.log(theme);
-
+  const [selected, setSelected] = useState(0);
+  const { onToggleMode } = useSettings();
   return (
     <>
       <Box
         p={2}
         sx={{
           backgroundColor: theme.palette.background.paper,
-          boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+          boxShadow: "0px 0px 2px rgba(0,0,0,0.25)",
           height: "100vh",
           width: 100,
         }}
       >
         <Stack
           direction="column"
-          alignItems={"center"}
+          allignItems={"center"}
           sx={{ width: "100%" }}
           spacing={3}
         >
-          <Box
-          p ={1}
-            sx={{
-              backgroundColor: theme.palette.primary.main,
-              height: 64,
-              width: 64,
-              borderRadius: 1.5,
-            }}
-          >
-            <img src={Logo} alt={"Chat app logo"} />
-          </Box>
-          <Stack spacing={3}>
-            sx={{ width: "max-content" }}
-            direction="column"
-            alignItems={"center"}
-            spacing={3}
-          
-            {Nav_Buttons.map((el) => (
-              el.index === selected?
-              <Box
-              p ={1}
-                sx={{
-                  backgroundColor: theme.palette.primary.main,
-                  borderRadius: 1.5,
-                }}
-              >
-                <IconButton
-                  sx={{ width: "max-content", color: "#fff" }}
-                  key={el.index}
-                >
-                  {el.icon}
-                </IconButton>
-              </Box>
-             ) :(<IconButton
-              onClick ={()=>{
-                setSelected(el.index)
+          <Stack alignItems={"center"} spacing={4}>
+            <Box
+              sx={{
+                backgroundColor: theme.palette.primary.main,
+                height: 64,
+                width: 64,
+                borderRadius: 1.5,
               }}
-              sx={{ width: "max-content", color: "#000" }}
-              key={el.index}
             >
-              {el.icon}
-            </IconButton>
-            ))}
-  
-          <Divider sx = {{width:"48px"}} />
-          { selected ===3 ?
-           <Box
-           p ={1}
-             sx={{
-               backgroundColor: theme.palette.primary.main,
-               borderRadius: 1.5,
-             }}
-           > 
-           <iconButton sx={{ width: "max-content", color:"fff"}}>
-            <Gear />
-           </iconButton>
-           </Box>
-           ):(
-           <IconButton onClick={()=>{
-            setSelected(3);
-           }}
-           sx={{ width: "max-content", color: "#000" }}
-           >
-           <Gear />
-         </IconButton>
-          )
-          
-</Stack>
-<Avatar src = ></Avatar>
-</Stack>
-</Box>
-<Outlet />
-</>
-)} ;
+              <img src={logo} alt={"chat logo"} />
+            </Box>
+            <Stack
+              // sx={{ width: "max-content" }}
+              direction={"column"}
+              allignItems={"center"}
+              justfyContent="space-between"
+              sx={{ height: "100%" }}
+              spacing={3}
+            >
+              {Nav_Buttons.map((el) =>
+                el.index === selected ? (
+                  <Box
+                    p={1} // did it to allign the other button right below the logo in center
+                    sx={{
+                      backgroundColor: theme.palette.primary.main,
+                      borderRadius: 1.5,
+                    }}
+                  >
+                    <IconButton
+                      sx={{ width: "max-content", color: "#fff" }}
+                      key={el.index}
+                    >
+                      {el.icon}
+                    </IconButton>
+                  </Box>
+                ) : (
+                  <IconButton
+                    onClick={() => {
+                      setSelected(el.index);
+                    }}
+                    sx={{ width: "max-content", color: "#000" }}
+                    key={el.index}
+                  >
+                    {el.icon}
+                  </IconButton>
+                )
+              )}
+              <Divider sx={{ width: "48px" }} />
+              {selected == 3 ? (
+                <Box
+                  p={1} // did it to allign the other button right below the logo in center
+                  sx={{
+                    backgroundColor: theme.palette.primary.main,
+                    borderRadius: 1.5,
+                  }}
+                >
+                  <IconButton sx={{ width: "max-content", color: "#fff" }}>
+                    <Gear />
+                  </IconButton>
+                </Box>
+              ) : (
+                <IconButton
+                  onClick={() => {
+                    setSelected(3);
+                  }}
+                  sx={{ width: "max-content", color: "#000" }}
+                >
+                  <Gear />
+                </IconButton>
+              )}
+            </Stack>
+          </Stack>
+
+          <Stack spacing={35}>
+            <Avatar src={faker.image.avatar()} />
+            <Switch
+              onChange=
+              {() => {
+                onToggleMode();
+              }}
+              defaultChecked
+            />
+          </Stack>
+        </Stack>
+      </Box>
+      <Outlet />
+    </>
+  );
+};
 
 export default DashboardLayout;
